7ee48f4dc7cf48d85914d9c5d04030fecafdc016
Simon Vitt <ttivnomis@gmail.com>
2022-12-06 12:11:38 +0100

    progressBar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let namesPokemon = [];
 let isLoading = false;
 let pokemonMatches = [];
 let searchbarOpen = false;
+let currentPokemonAmount = 0;
+let wantedPokemonAmount = 31;
 
 async function render() {
     await getAllNames();
@@ -21,13 +23,23 @@ async function getAllNames() {
 
 /*show all pokemon in overview, all beeing small cards, loadsscreen while loading*/
 async function showAllPokemon() {
-    isLoading =true;
+    isLoading = true;
     loadingScreen();
-    for (let i = 0; i < namesPokemon.length; i++) {
+    for (let i = currentPokemonAmount; i < wantedPokemonAmount; i++) {
         await showPokemonOverview(namesPokemon[i], i + 1);
+        updateProgressBar(i, currentPokemonAmount, wantedPokemonAmount);
     }
+    changePokemonAmount();
     isLoading = false;
     loadingScreen();
+}
+
+/*change wanted and currentPokemonAmount to load more pokemon when scroll detected*/
+function changePokemonAmount() {
+    currentPokemonAmount = wantedPokemonAmount;
+    if (wantedPokemonAmount < 151) {
+        wantedPokemonAmount += 30;
+    }
 }
 
 /*show all pokemon in overview (image, name, id*/
@@ -41,13 +53,12 @@ async function showPokemonOverview(pokemonName, number) {
 }
 
 /*show and remove loadingscreen*/
-function loadingScreen(){
-    if(isLoading){
+function loadingScreen() {
+    if (isLoading) {
         document.getElementById('loadingScreen').classList.remove('d-none');
-        document.getElementById('containerPokemon').classList.add('d-none');
-    }else{
+    } else {
         document.getElementById('loadingScreen').classList.add('d-none');
-        document.getElementById('containerPokemon').classList.remove('d-none');
+        resetProgressBar();
     }
 }
 
@@ -70,7 +81,7 @@ async function showStats(number) {
 }
 
 /*show details of pokemon in single cardview*/
-function showDetails(pokemonDetails, number){
+function showDetails(pokemonDetails, number) {
     pokemonName = pokemonDetails['species']['name'].charAt(0).toUpperCase() + pokemonDetails['species']['name'].slice(1);
     document.getElementById('containerStatsPokemon').classList.remove('d-none');
     document.getElementById('containerStatsPokemon').innerHTML = '';
@@ -80,7 +91,7 @@ function showDetails(pokemonDetails, number){
     document.getElementById('weightDiv').innerHTML += pokemonDetails['weight'];
 }
 
-function showType(types){
+function showType(types) {
     for (let i = 0; i < types.length; i++) {
         const type = types[i]['type']['name'];
         document.getElementById('divTypes').innerHTML += pokemonTypesTemplate(type, i);
@@ -91,8 +102,8 @@ function showType(types){
     }
 }
 
-function showBaseStats(stats){
-    for(let i = 0; i < stats.length; i++){
+function showBaseStats(stats) {
+    for (let i = 0; i < stats.length; i++) {
         const number = stats[i]['base_stat'];
         const name = stats[i]['stat']['name'];
         document.getElementById('baseStats').innerHTML += pokemonStatsTemplate(name, number);
@@ -100,19 +111,19 @@ function showBaseStats(stats){
 }
 
 /*in single cardview left arrow clicked to see single cardview of last pokemon in line after id*/
-function lastPokemon(number){
-    if(number > 1){
+function lastPokemon(number) {
+    if (number > 1) {
         showStats(number - 1);
-    }else{
+    } else {
         document.getElementById('containerStatsPokemon').classList.add('d-none');
     }
 }
 
 /*in single cardview right arrow clicked to see single cardview of next pokemon in line after id*/
-function nextPokemon(number){
-    if(number < 151){
+function nextPokemon(number) {
+    if (number < 151) {
         showStats(number + 1);
-    }else{
+    } else {
         document.getElementById('containerStatsPokemon').classList.add('d-none');
     }
 }
@@ -122,35 +133,35 @@ function closeStats() {
     document.getElementById('containerStatsPokemon').classList.add('d-none');
 }
 
-function dontClose(event){
+function dontClose(event) {
     event.stopPropagation();
 }
 
 /*search for a pokemon (name)*/
-async function searchPokemon(){
+async function searchPokemon() {
     pokemonMatches = [];
     let input = document.getElementById('inputSearch').value.toLowerCase();
     document.getElementById('containerPokemon').innerHTML = '';
-    if(input == ''){
+    if (input == '') {
         showAllPokemon();
-    }else{
+    } else {
         comparePokemonNames(input);
         await showSearchedPokemons();
     }
 }
 
 /*compare names after every char*/
-function comparePokemonNames(input){
-    for(let i = 0; i < namesPokemon.length; i++){
+function comparePokemonNames(input) {
+    for (let i = 0; i < namesPokemon.length; i++) {
         let name = namesPokemon[i];
-        for(let j = 0; j < input.length; j++){
-            if(name.charAt(j) == input.charAt(j)){
-                if(pokemonMatches.indexOf(name) == -1){
+        for (let j = 0; j < input.length; j++) {
+            if (name.charAt(j) == input.charAt(j)) {
+                if (pokemonMatches.indexOf(name) == -1) {
                     pokemonMatches.push(name);
                 }
-            }else{
+            } else {
                 const index = pokemonMatches.indexOf(name);
-                if(index != -1){
+                if (index != -1) {
                     pokemonMatches.splice(index, 1);
                 }
                 break;
@@ -160,26 +171,52 @@ function comparePokemonNames(input){
 }
 
 /*show all matching pokemon after search details*/
-async function showSearchedPokemons(){
-    isLoading =true;
+async function showSearchedPokemons() {
+    isLoading = true;
     loadingScreen();
     for (let i = 0; i < pokemonMatches.length; i++) {
         const pokemonDetails = await getData(`${pokemonMatches[i]}`);
         await showPokemonOverview(pokemonMatches[i], pokemonDetails['id']);
+        updateProgressBar(i, 0, pokemonMatches.length);
     }
+    currentPokemonAmount = 0;
+    wantedPokemonAmount = 31;
     isLoading = false;
     loadingScreen();
 }
 
 /*show and hide searchbar in smaller devices*/
-function showSearchBar(){
-    if(searchbarOpen){
+function showSearchBar() {
+    if (searchbarOpen) {
         document.getElementById('inputSearch').classList.add('hide450');
         searchbarOpen = false;
         document.getElementById('seachBarOpener').innerHTML = '&#9660';
-    }else{
+    } else {
         document.getElementById('inputSearch').classList.remove('hide450');
         searchbarOpen = true;
         document.getElementById('seachBarOpener').innerHTML = '&#128473';
     }
+}
+
+/*load more pokemon on scroll*/
+async function detectScroll() {
+    let maxHeight = document.body.scrollHeight - window.innerHeight;
+    let scrollDownPercent = window.scrollY / maxHeight;
+    if (scrollDownPercent > 0.9 && isLoading == false) {
+        await showAllPokemon();
+    }
+}
+
+/*show progressbar while loading pokemon*/
+function updateProgressBar(i, currentPokemonAmount, wantedPokemonAmount) {
+    let goal = wantedPokemonAmount - currentPokemonAmount;
+    i = i - currentPokemonAmount;
+    let progress = Math.round((i / goal) * 100);
+    document.getElementById('progress-bar').innerHTML = `${progress}%`;
+    document.getElementById('progress-bar').style = `width: ${progress}%;`;
+}
+
+function resetProgressBar() {
+    document.getElementById('progress-bar').innerHTML = `0%`;
+    document.getElementById('progress-bar').style = `width: 0%;`;
 }
